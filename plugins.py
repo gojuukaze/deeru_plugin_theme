b746b695598567c2322ec35b886a891845837357
gojuukaze <fengzhucezhuanyong@outlook.com>
2018-07-13 16:20:13 +0800

    Update plugins.py

--- plugins.py
+++ plugins.py
@@ -1,9 +1,18 @@
 [
     {
         'name':'deeru-qiniu',
+        'nice_name':'deeru七牛插件'
         'url':'https://github.com/gojuukaze/deeru-qiniu',
         'author':'gojuukaze'
         'desc':'deeru七牛插件',
+    },
+    
+     {
+        'name':'deeru-api',
+        'nice_name':'api插件'
+        'url':'https://github.com/gojuukaze/deeru-api',
+        'author':'gojuukaze'
+        'desc':'DeerU接口扩展，返回json数据',
     },
 
 ]
